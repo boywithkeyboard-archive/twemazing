4e12ab76ed6dc5678a32be808d8ef97cd4d6e756
Azury <azury.wtf@gmail.com>
2021-12-30 18:22:25 +0100

    added `scss` builder

---
+++ src/builders/scss.mjs
@@ -0,0 +1,84 @@
+import { writeFile, mkdir } from 'fs/promises'
+import config from '../config.mjs'
+import { copy, emptyDir } from 'fs-extra'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import log from '../logger.mjs'
+import emojis from '../emojis.mjs'
+import { existsSync } from 'fs'
+import CleanCSS from 'clean-css'
+
+(async () => {
+  const __dirname = dirname(fileURLToPath(import.meta.url))
+
+  // clear directory
+  if (existsSync(path.join(__dirname, '../../packages/scss'))) {
+    await emptyDir(path.join(__dirname, '../../packages/scss'))
+  } else {
+    await mkdir(path.join(__dirname, '../../packages/scss'))
+  }
+
+  config.keywords.push('scss')
+  config.keywords.push('stylesheet')
+
+  // generate package.json
+  await log('Generating package.json...')
+  await writeFile(path.join(__dirname, '../../packages/scss/package.json'), JSON.stringify({
+    name: '@twemazing/scss',
+    version: config.version,
+    description: config.description,
+    main: 'any.min.scss',
+    browser: 'any.min.scss',
+    funding: config.funding,
+    repository: config.repository,
+    keywords: config.keywords,
+    author: config.author,
+    license: config.license,
+    bugs: config.bugs,
+    homepage: config.homepage
+  }))
+
+  // copy license
+  await log('Copying the license...')
+  await copy(path.join(__dirname, '../../license'), path.join(__dirname, '../../packages/scss/license'))
+
+  // copy readme
+  await log('Copying the readme...')
+  await copy(path.join(__dirname, '../../readme.md'), path.join(__dirname, '../../packages/scss/readme.md'))
+
+  // generate scss
+  await log('Generating the stylesheet...')
+let stylesheet = `
+
+@mixin emoji($key) {
+  background: url('https://cdn.jsdelivr.net/npm/@twe/x/#{$key}.svg');
+}
+
+.twe {
+  display: inline-block;
+  height: 1em;
+  width: 1em;
+  margin: 0 .05em 0 .1em;
+  vertical-align: -0.1em;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 1em 1em;
+}
+`
+
+for (const emoji of emojis) {
+stylesheet += `
+.twe-${emoji.shortName.toLowerCase().replaceAll(' ', '-')} {
+  @include emoji('${emoji.codepoint}');
+}
+`
+}
+  stylesheet = config.head + stylesheet
+
+  await writeFile(path.join(__dirname, '../../packages/scss/any.scss'), stylesheet)
+
+  // generate minified stylesheet
+  await log('Generating minified stylesheet...')
+  const minifiedStylesheet = new CleanCSS().minify(stylesheet)
+  await writeFile(path.join(__dirname, '../../packages/scss/any.min.scss'), minifiedStylesheet.styles.replace(config.head, config.minifiedHead + '\n'), 'utf-8')
+})()
