f1c4755cd38bfddd9482e3225bf37a15890efab2
Azury <azury.wtf@gmail.com>
2021-12-30 16:58:11 +0100

    edited config

---
+++ src/config.mjs
@@ -0,0 +1,25 @@
+export default {
+  version: '0.0.0',
+  description: '🧸 A better Twemoji Library',
+  funding: {
+    type: 'individual',
+    url: 'https://ko-fi.com/azury'
+  },
+  repository: {
+    type: 'git',
+    url: 'git+https://github.com/azurydev/twemazing.git'
+  },
+  keywords: [
+    'twemazing',
+    'twemoji',
+    'emojis',
+    'easy-to-use',
+    'unicode'
+  ],
+  author: 'Azury <hello@azury.dev> (https://azury.dev)',
+  license: 'Apache-2.0',
+  bugs: {
+    url: 'https://github.com/azurydev/twemazing/issues'
+  },
+  homepage: 'https://twemazing.com'
+}
