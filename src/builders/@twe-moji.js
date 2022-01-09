6ce5be0a7733d11873f7d54d893edb49507514d6
Azury <azury.wtf@gmail.com>
2022-01-09 19:24:49 +0100

    added new builder for `@twe/moji` package

---
+++ src/builders/@twe-moji.js
@@ -0,0 +1,32 @@
+import { writeFile } from 'fs/promises'
+import { copy } from 'fs-extra'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+export default async ({ release }) => {
+  const __dirname = dirname(fileURLToPath(import.meta.url))
+
+  // add keywords
+  release.keywords.push('cdn')
+  release.keywords.push('images')
+
+  // generate package.json
+  await writeFile(path.join(__dirname, '../../packages/@twe-moji/package.json'), JSON.stringify({
+    name: '@twe/moji',
+    version: release.version,
+    description: release.description,
+    funding: release.funding,
+    repository: release.repository,
+    keywords: release.keywords,
+    author: release.author,
+    license: release.license,
+    bugs: release.bugs,
+    homepage: release.homepage
+  }))
+
+  // copy license
+  await copy(path.join(__dirname, '../../license'), path.join(__dirname, '../../packages/@twe-moji/license'))
+
+  // copy readme
+  await copy(path.join(__dirname, '../../readme.md'), path.join(__dirname, '../../packages/@twe-moji/readme.md'))
+}
